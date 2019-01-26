3bef764db295e6d03f71b53ee70c0be616225984
OGUZ_OZYURT <prfoguz@gmail.com>
2019-01-25 18:29:51 -0600

    updated

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    //this is a good idea
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+
+        return num1 - num2;
+    }
+
+    public static double mutliply(double num1, double num2) {
+
+        return num1 * num2;
+    }
+    // LOCAL REPO : COMMENT 1
+    public static double divide(double n1, double n2) {
+        double result = n1 / n2;
+        return result;
+    //this is a comment in GitHub 2
+
+    }
+
+}
